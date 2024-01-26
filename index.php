78ff434b582a5390534dda85e4315770b9b93292
moonymel <musumeci.me@gmail.com>
2024-01-26 16:10:12 +0100

    [ADD] file functions.php and updated - MILESTONE 2 COMPLETATA

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
-<?php
-
-    function createPassword($num) {
-
-        $lower_char = 'abcdefghijklmnopqrstuvwxyz';
-        $upper_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $num_char = '0123456789';
-        $spec_char = '.-@,;:*';
-        $all_char = $lower_char.$upper_char.$num_char.$spec_char;
-
-        $password = '';
-
-        for($i=0; $i<$num; $i++) {
-            $password .= $all_char[rand(0, strlen($all_char)-1)];
-        }
-        
-        return $password;
-    }
-
-    if(isset($_GET['psw_len'])){
-
-        $psw_len = (int)$_GET['psw_len'];
-        $new_pass = createPassword($psw_len);
-
-    }
-
-
+<?php 
+    include __DIR__.'/partials/functions.php';
 ?>
 
 <!DOCTYPE html>
